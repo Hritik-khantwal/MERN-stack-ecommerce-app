eb6ec2a47baa16fe4ac05254ff578538c97ece4a
Hritik-khantwal <147543272+Hritik-khantwal@users.noreply.github.com>
2024-02-23 16:53:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import categoryRoutes from "./routes/CategoryRoutes.js";
 import productRoutes from "./routes/productRoute.js";
 import ratingRoutes from "./routes/ratingRoute.js";
 import path from "path";
-import {fileUrlToPath} from "url";
+import {fileURLToPath} from "url";
 
 // configure env
 dotenv.config();
@@ -18,7 +18,7 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-const __filename = fileUrlToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // rest object
