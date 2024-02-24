0be230aefbb69569b0938d74f8aaab378ea43fd9
Hritik-khantwal <hritikkhantwal25@gmail.com>
2024-02-24 17:57:33 +0530

    updated mongo db connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import connectDB from "./config/db.js";
+// import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/CategoryRoutes.js";
@@ -10,12 +10,24 @@ import productRoutes from "./routes/productRoute.js";
 import ratingRoutes from "./routes/ratingRoute.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
 // configure env
 dotenv.config();
 
+// PORT
+const PORT = process.env.PORT || 8080;
+
 // databse config
-connectDB();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.bgMagenta.white);
+  } catch (error) {
+    console.log(`Error in MongoDB ${error}`.bgRed.white);
+    process.exit(1);
+  }
+};
 
 //esmodule fix
 const __filename = fileURLToPath(import.meta.url);
@@ -35,15 +47,20 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/rating", ratingRoutes);
+
 // rest api
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
-// PORT
-const PORT = process.env.PORT || 8080;
+// server listen
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is working at port ${PORT}`.bgCyan.white);
+  });
+});
 
 // server listen
-app.listen(PORT, () => {
-  console.log(`server is working at port ${PORT}`.bgCyan.white);
-});
+// app.listen(PORT, () => {
+//   console.log(`server is working at port ${PORT}`.bgCyan.white);
+// });
